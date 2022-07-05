2bc05f7c47dd585c09d8c2b3136a28829924c5db
Rashmi Bora <you@example.com>
2022-07-05 22:43:40 +0530

    third

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  @Input() public shopData:any;
-  @Input() public plus:any;
+  @Input() public shopData: any;
+  @Input() public parent: any;
+  @Input() public cart: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  add1(food: any): void {
+    //console.log(food.qty1);
+    if (food.qty1 !== 10) {
+      food.qty1 = food.qty1 + 1;
+      this.cart = this.cart + 1;
+      console.log(this.cart);
+    }
   }
 
+  sub1(food: any): void {
+    //console.log(food.qty1);
+    if (food.qty1 !== 0) {
+      food.qty1 = food.qty1 - 1;
+      this.cart = this.cart - 1;
+    }
+      
+     else if (food.qty1 === 0) {
+        //delete this.shopData;
+        //this.shopData.pop();
+        this.shopData.splice(this.shopData.indexOf(this.shopData),1);
+      
+      }
+      
+  }
 }
+ 
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -9,65 +9,60 @@ export class ShopComponent implements OnInit {
   box1 = 'box1';
   cartItem: number = 0;
   showMe = true;
+  cartArray: Array<{img1: any, amt1: number, qty1: number}> = []; 
   foodArray = [
       {
-        id:1,
-        img:"../../assets/gulab.jpg",
-        amt:100,
-        qty:1
+        img: '../../assets/gulab.jpg',
+        amt: 100,
+        qty: 1
       },
       {
-        id:2,
-        img:"../../assets/dosa.jpg",
-        amt:200,
-        qty:1
+        img: '../../assets/dosa.jpg',
+        amt: 200,
+        qty: 1
       },
       {
-        id:3,
-        img:"../../assets/noodles.jpg",
-        amt:250,
-        qty:1
+        img: '../../assets/noodles.jpg',
+        amt: 250,
+        qty: 1
       },
       {
-        id:4,
-        img:"../../assets/pav-bhaji.jpg",
-        amt:150,
-        qty:1
+        img: '../../assets/pav-bhaji.jpg',
+        amt: 150,
+        qty: 1
       },
       {
-        id:5,
-        img:"../../assets/khadvi.jpg",
-        amt:110,
-        qty:1
+        img: '../../assets/khadvi.jpg',
+        amt: 110,
+        qty: 1
       },
   ];
-
-  onToggle(): void{
-    this.showMe = !this.showMe;
-   
-  }
  
-  add(f:any): void{
-    //console.log(f.qty);
-    if(f.qty!=10){
-    f.qty = f.qty + 1;
-    }
-  }
-
-  minus(f:any): void{
-    if(f.qty !=1){
-    f.qty = f.qty - 1;
-    }
-  }
-
-  cartArray:any = [];
-  addCart(f:any){
-  this.cartItem = this.cartItem + f.qty;
-  }
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onToggle(): void{
+    this.showMe = !this.showMe;  
+  }
+ 
+  add(foodVar:any): void{
+    //console.log(f.qty);
+    if(foodVar.qty!==10){
+      foodVar.qty = foodVar.qty + 1;
+    }
+  }
+
+  minus(foodVar:any): void{
+    if(foodVar.qty !==1){
+      foodVar.qty = foodVar.qty - 1;
+    }
+  }
+
+  addCart(foodVar:any){
+  this.cartItem = this.cartItem + foodVar.qty;
+  this.cartArray.push({img1:foodVar.img,amt1:foodVar.amt, qty1:foodVar.qty});
+  return this.cartArray; 
+  }
 }
